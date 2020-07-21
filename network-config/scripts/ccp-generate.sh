816156cdaa5db67f0ba0012fd5442c3e23de150c
Arnab Mukherjee <mukherjeearnab905@gmail.com>
2020-07-21 14:44:29 +0530

    completed cpp-generate

--- network-config/scripts/ccp-generate.sh
+++ network-config/scripts/ccp-generate.sh
@@ -1,21 +1,20 @@
 #!/bin/bash
 
-function one_line_pem {
-    echo "`awk 'NF {sub(/\\n/, ""); printf "%s\\\\\\\n",$0;}' $1`"
+function one_line_pem() {
+    echo "$(awk 'NF {sub(/\\n/, ""); printf "%s\\\\\\\n",$0;}' $1)"
 }
 
-function json_ccp {
+function json_ccp() {
     local PP=$(one_line_pem $5)
     local CP=$(one_line_pem $6)
     sed -e "s/\${ORG}/$1/" \
-        -e "s/\${ORGMSP}/$2/" \
-        -e "s/\${P0PORT}/$3/" \
-        -e "s/\${CAPORT}/$4/" \
-        -e "s#\${PEERPEM}#$PP#" \
-        -e "s#\${CAPEM}#$CP#" \
-        ../../connections/ccp-template.json 
+    -e "s/\${ORGMSP}/$2/" \
+    -e "s/\${P0PORT}/$3/" \
+    -e "s/\${CAPORT}/$4/" \
+    -e "s#\${PEERPEM}#$PP#" \
+    -e "s#\${CAPEM}#$CP#" \
+    ../../connections/ccp-template.json
 }
-
 
 ORG=citizen
 ORGMSP=Citizen
@@ -24,39 +23,39 @@ CAPORT=7054
 PEERPEM=../crypto-config/peerOrganizations/citizen.vehicle.com/tlsca/tlsca.citizen.vehicle.com-cert.pem
 CAPEM=../crypto-config/peerOrganizations/citizen.vehicle.com/ca/ca.citizen.vehicle.com-cert.pem
 
-echo "$(json_ccp $ORG $ORGMSP $P0PORT $CAPORT $PEERPEM $CAPEM)" > ../../connections/connection-citizen.json
+echo "$(json_ccp $ORG $ORGMSP $P0PORT $CAPORT $PEERPEM $CAPEM)" >../../connections/connection-citizen.json
+
+ORG=rto
+ORGMSP=RTO
+P0PORT=8051
+CAPORT=8054
+PEERPEM=../crypto-config/peerOrganizations/rto.vehicle.com/tlsca/tlsca.rto.vehicle.com-cert.pem
+CAPEM=../crypto-config/peerOrganizations/rto.vehicle.com/ca/ca.rto.vehicle.com-cert.pem
+
+echo "$(json_ccp $ORG $ORGMSP $P0PORT $CAPORT $PEERPEM $CAPEM)" >../../connections/connection-rto.json
+ORG=pollution
+ORGMSP=Pollution
+P0PORT=9051
+CAPORT=9054
+PEERPEM=../crypto-config/peerOrganizations/pollution.vehicle.com/tlsca/tlsca.pollution.vehicle.com-cert.pem
+CAPEM=../crypto-config/peerOrganizations/pollution.vehicle.com/ca/ca.pollution.vehicle.com-cert.pem
+
+echo "$(json_ccp $ORG $ORGMSP $P0PORT $CAPORT $PEERPEM $CAPEM)" >../../connections/connection-pollution.json
+
+ORG=insurance
+ORGMSP=Insurance
+P0PORT=10051
+CAPORT=10054
+PEERPEM=../crypto-config/peerOrganizations/insurance.vehicle.com/tlsca/tlsca.insurance.vehicle.com-cert.pem
+CAPEM=../crypto-config/peerOrganizations/insurance.vehicle.com/ca/ca.insurance.vehicle.com-cert.pem
+
+echo "$(json_ccp $ORG $ORGMSP $P0PORT $CAPORT $PEERPEM $CAPEM)" >../../connections/connection-insurance.json
 
 ORG=police
 ORGMSP=Police
-P0PORT=8051
-CAPORT=8054
+P0PORT=11051
+CAPORT=11054
 PEERPEM=../crypto-config/peerOrganizations/police.vehicle.com/tlsca/tlsca.police.vehicle.com-cert.pem
 CAPEM=../crypto-config/peerOrganizations/police.vehicle.com/ca/ca.police.vehicle.com-cert.pem
 
-echo "$(json_ccp $ORG $ORGMSP $P0PORT $CAPORT $PEERPEM $CAPEM)" > ../../connections/connection-police.json
-ORG=forensics
-ORGMSP=Forensics
-P0PORT=9051
-CAPORT=9054
-PEERPEM=../crypto-config/peerOrganizations/forensics.vehicle.com/tlsca/tlsca.forensics.vehicle.com-cert.pem
-CAPEM=../crypto-config/peerOrganizations/forensics.vehicle.com/ca/ca.forensics.vehicle.com-cert.pem
-
-echo "$(json_ccp $ORG $ORGMSP $P0PORT $CAPORT $PEERPEM $CAPEM)" > ../../connections/connection-forensics.json
-
-ORG=court
-ORGMSP=Court
-P0PORT=10051
-CAPORT=10054
-PEERPEM=../crypto-config/peerOrganizations/court.vehicle.com/tlsca/tlsca.court.vehicle.com-cert.pem
-CAPEM=../crypto-config/peerOrganizations/court.vehicle.com/ca/ca.court.vehicle.com-cert.pem
-
-echo "$(json_ccp $ORG $ORGMSP $P0PORT $CAPORT $PEERPEM $CAPEM)" > ../../connections/connection-court.json
-
-ORG=identityprovider
-ORGMSP=IdentityProvider
-P0PORT=11051
-CAPORT=11054
-PEERPEM=../crypto-config/peerOrganizations/identityprovider.vehicle.com/tlsca/tlsca.identityprovider.vehicle.com-cert.pem
-CAPEM=../crypto-config/peerOrganizations/identityprovider.vehicle.com/ca/ca.identityprovider.vehicle.com-cert.pem
-
-echo "$(json_ccp $ORG $ORGMSP $P0PORT $CAPORT $PEERPEM $CAPEM)" > ../../connections/connection-identityprovider.json
+echo "$(json_ccp $ORG $ORGMSP $P0PORT $CAPORT $PEERPEM $CAPEM)" >../../connections/connection-police.json
