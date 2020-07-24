ce162289b06cc44008ba2f597825b768e162ead2
Arnab Mukherjee <mukherjeearnab905@gmail.com>
2020-07-24 10:51:50 +0530

    added vehicle_cc

--- chaincode/vehicle_cc/chaincode.go
+++ chaincode/vehicle_cc/chaincode.go
@@ -15,15 +15,28 @@ import (
 type Chaincode struct {
 }
 
-// Definition of the Evidence structure
-type evidence struct {
-	Type            string `json:"Type"`
-	ID              string `json:"ID"`
-	MimeType        string `json:"MimeType"`
-	Extention       string `json:"Extention"`
-	Description     string `json:"Description"`
-	DateTime        int    `json:"DateTime"`
-	InvestigationID string `json:"InvestigationID"`
+type ownership struct {
+	Prev     string `json:"Prev"`
+	Curr     string `json:"Curr"`
+	Employee string `json:"Employee"`
+	Date     int    `json:"Date"`
+}
+
+// Definition of the Vehicle structure
+type vehicle struct {
+	Type             string      `json:"Type"`
+	RegNo            string      `json:"RegNo"`
+	Make             string      `json:"Make"`
+	Model            string      `json:"Model"`
+	ModelVariant     string      `json:"ModelVariant"`
+	ModelYear        string      `json:"ModelYear"`
+	Color            string      `json:"Color"`
+	EngineNo         string      `json:"EngineNo"`
+	ChassisNo        string      `json:"ChassisNo"`
+	Owner            string      `json:"Owner"`
+	Insurance        []string    `json:"Insurance"`
+	PollutionCert    []string    `json:"PollutionCert"`
+	OwnershipHistory []ownership `json:"OwnershipHistory"`
 }
 
 // Init is called when the chaincode is instantiated by the blockchain network.
@@ -36,74 +49,83 @@ func (cc *Chaincode) Invoke(stub shim.ChaincodeStubInterface) sc.Response {
 	fcn, params := stub.GetFunctionAndParameters()
 	fmt.Println("Invoke()", fcn, params)
 
-	if fcn == "addEvidence" {
-		return cc.addEvidence(stub, params)
-	} else if fcn == "readEvidence" {
-		return cc.readEvidence(stub, params)
+	if fcn == "createVehicleProfile" {
+		return cc.createVehicleProfile(stub, params)
+	} else if fcn == "getVehicleProfile" {
+		return cc.getVehicleProfile(stub, params)
+	} else if fcn == "addPollutionCertificate" {
+		return cc.addPollutionCertificate(stub, params)
+	} else if fcn == "addInsurancePolicy" {
+		return cc.addInsurancePolicy(stub, params)
+	} else if fcn == "transferOwnership" {
+		return cc.transferOwnership(stub, params)
 	} else {
 		fmt.Println("Invoke() did not find func: " + fcn)
 		return shim.Error("Received unknown function invocation!")
 	}
 }
 
-// Function to add new Evidence
-func (cc *Chaincode) addEvidence(stub shim.ChaincodeStubInterface, params []string) sc.Response {
+// Function to add new Vehicle
+func (cc *Chaincode) createVehicleProfile(stub shim.ChaincodeStubInterface, params []string) sc.Response {
 	// Check Access
-	creatorOrg, creatorCertIssuer, err := getTxCreatorInfo(stub)
-	if !authenticatePoFoCi(creatorOrg, creatorCertIssuer) {
+	creatorOrg, creatorCertIssuer, creator, err := getTxCreatorInfo(stub)
+	if !authenticateRTO(creatorOrg, creatorCertIssuer) {
 		return shim.Error("{\"Error\":\"Access Denied!\",\"Payload\":{\"MSP\":\"" + creatorOrg + "\",\"CA\":\"" + creatorCertIssuer + "\"}}")
 	}
 
 	// Check if sufficient Params passed
-	if len(params) != 6 {
-		return shim.Error("Incorrect number of arguments. Expecting 2")
+	if len(params) != 9 {
+		return shim.Error("Incorrect number of arguments. Expecting 9")
 	}
 
 	// Check if Params are non-empty
-	for a := 0; a < 6; a++ {
+	for a := 0; a < 9; a++ {
 		if len(params[a]) <= 0 {
 			return shim.Error("Argument must be a non-empty string")
 		}
 	}
 
-	ID := params[0]
-	MimeType := params[1]
-	Extention := params[2]
-	Description := params[3]
-	DateTime := params[4]
-	InvestigationID := params[5]
-	DateTimeI, err := strconv.Atoi(DateTime)
+	RegNo := params[0]
+	Make := params[1]
+	Model := params[2]
+	ModelVariant := params[3]
+	ModelYear := params[4]
+	Color := params[5]
+	EngineNo := params[6]
+	ChassisNo := params[7]
+	Owner := params[8]
+	var Insurance []string
+	var PollutionCert []string
+	var OwnershipHistory []ownership
+
+	// Check if Vehicle exists with Key => params[0]
+	vehicleAsBytes, err := stub.GetState(RegNo)
 	if err != nil {
-		return shim.Error("Error: Invalid DateTime!")
+		return shim.Error("Failed to check if Vehicle exists!")
+	} else if vehicleAsBytes != nil {
+		return shim.Error("Vehicle Already Exists!")
 	}
 
-	// Check if Evidence exists with Key => params[0]
-	evidenceAsBytes, err := stub.GetState(ID)
-	if err != nil {
-		return shim.Error("Failed to check if Evidence exists!")
-	} else if evidenceAsBytes != nil {
-		return shim.Error("Evidence Already Exists!")
-	}
+	// Generate Vehicle from params provided
+	vehicle := &vehicle{"VEHCL",
+		RegNo, Make, Model, ModelVariant, ModelYear, Color,
+		EngineNo, ChassisNo, Owner, Insurance, PollutionCert, OwnershipHistory}
 
-	// Generate Evidence from params provided
-	evidence := &evidence{"evidence",
-		ID, MimeType, Extention, Description, DateTimeI, InvestigationID}
-
-	// Get JSON bytes of Evidence struct
-	evidenceJSONasBytes, err := json.Marshal(evidence)
-	if err != nil {
-		return shim.Error(err.Error())
-	}
-
-	// Put State of newly generated Evidence with Key => params[0]
-	err = stub.PutState(ID, evidenceJSONasBytes)
+	// Get JSON bytes of Vehicle struct
+	vehicleJSONasBytes, err := json.Marshal(vehicle)
 	if err != nil {
 		return shim.Error(err.Error())
 	}
 
-	// Add Evidence ID to The Investigation with InvestigationID
-	args := util.ToChaincodeArgs("addEvidence", InvestigationID, ID)
-	response := stub.InvokeChaincode("investigation_cc", args, "mainchannel")
+	// Put State of newly generated Vehicle with Key => params[0]
+	err = stub.PutState(RegNo, vehicleJSONasBytes)
+	if err != nil {
+		return shim.Error(err.Error())
+	}
+
+	// Add Vehicle ID to The Citizen with UID
+	args := util.ToChaincodeArgs("addVehicle", Owner, RegNo)
+	response := stub.InvokeChaincode("profilemanager_cc", args, "mainchannel")
 	if response.Status != shim.OK {
 		return shim.Error(response.Message)
 	}
@@ -112,8 +134,8 @@ func (cc *Chaincode) addEvidence(stub shim.ChaincodeStubInterface, params []stri
 	return shim.Success(nil)
 }
 
-// Function to read an Evidence
-func (cc *Chaincode) readEvidence(stub shim.ChaincodeStubInterface, params []string) sc.Response {
+// Function to read an Vehicle
+func (cc *Chaincode) getVehicleProfile(stub shim.ChaincodeStubInterface, params []string) sc.Response {
 	// Check if sufficient Params passed
 	if len(params) != 1 {
 		return shim.Error("Incorrect number of arguments. Expecting 2")
@@ -124,18 +146,201 @@ func (cc *Chaincode) readEvidence(stub shim.ChaincodeStubInterface, params []str
 		return shim.Error("1st argument must be a non-empty string")
 	}
 
-	// Get State of Evidence with Key => params[0]
-	evidenceAsBytes, err := stub.GetState(params[0])
+	// Get State of Vehicle with Key => params[0]
+	vehicleAsBytes, err := stub.GetState(params[0])
 	if err != nil {
 		jsonResp := "{\"Error\":\"Failed to get state for " + params[0] + "\"}"
 		return shim.Error(jsonResp)
-	} else if evidenceAsBytes == nil {
-		jsonResp := "{\"Error\":\"Evidence does not exist!\"}"
+	} else if vehicleAsBytes == nil {
+		jsonResp := "{\"Error\":\"Vehicle does not exist!\"}"
 		return shim.Error(jsonResp)
 	}
 
 	// Returned on successful execution of the function
-	return shim.Success(evidenceAsBytes)
+	return shim.Success(vehicleAsBytes)
+}
+
+// Function to add new Pollution Certificate
+func (cc *Chaincode) addPollutionCertificate(stub shim.ChaincodeStubInterface, params []string) sc.Response {
+	// Check Access
+	creatorOrg, creatorCertIssuer, creator, err := getTxCreatorInfo(stub)
+	if !authenticatePollution(creatorOrg, creatorCertIssuer) {
+		return shim.Error("{\"Error\":\"Access Denied!\",\"Payload\":{\"MSP\":\"" + creatorOrg + "\",\"CA\":\"" + creatorCertIssuer + "\"}}")
+	}
+
+	// Check if sufficient Params passed
+	if len(params) != 2 {
+		return shim.Error("Incorrect number of arguments. Expecting 2")
+	}
+
+	// Check if Params are non-empty
+	for a := 0; a < 2; a++ {
+		if len(params[a]) <= 0 {
+			return shim.Error("Argument must be a non-empty string")
+		}
+	}
+
+	// Copy the Values from params[]
+	RegNo := params[0]
+	Cert := params[1]
+
+	// Check if Vehicle exists with Key => RegNo
+	vehicleAsBytes, err := stub.GetState(RegNo)
+	if err != nil {
+		return shim.Error("Failed to get Vehicle Details!")
+	} else if vehicleAsBytes == nil {
+		return shim.Error("Error: Vehicle Does NOT Exist!")
+	}
+
+	// Create Update struct var
+	vehicleToUpdate := vehicle{}
+	err = json.Unmarshal(vehicleAsBytes, &vehicleToUpdate) //unmarshal it aka JSON.parse()
+	if err != nil {
+		return shim.Error(err.Error())
+	}
+
+	// Update Vehicle.PollutionCert to append => Cert
+	vehicleToUpdate.PollutionCert = append(vehicleToUpdate.PollutionCert, Cert)
+
+	// Get JSON bytes of Vehicle struct
+	vehicleJSONasBytes, err := json.Marshal(vehicleToUpdate)
+	if err != nil {
+		return shim.Error(err.Error())
+	}
+
+	// Put State of newly generated Vehicle with Key => params[0]
+	err = stub.PutState(RegNo, vehicleJSONasBytes)
+	if err != nil {
+		return shim.Error(err.Error())
+	}
+
+	// Returned on successful execution of the function
+	return shim.Success(nil)
+}
+
+// Function to add new Insurance Policy
+func (cc *Chaincode) addInsurancePolicy(stub shim.ChaincodeStubInterface, params []string) sc.Response {
+	// Check Access
+	creatorOrg, creatorCertIssuer, creator, err := getTxCreatorInfo(stub)
+	if !authenticateInsurance(creatorOrg, creatorCertIssuer) {
+		return shim.Error("{\"Error\":\"Access Denied!\",\"Payload\":{\"MSP\":\"" + creatorOrg + "\",\"CA\":\"" + creatorCertIssuer + "\"}}")
+	}
+
+	// Check if sufficient Params passed
+	if len(params) != 2 {
+		return shim.Error("Incorrect number of arguments. Expecting 9")
+	}
+
+	// Check if Params are non-empty
+	for a := 0; a < 2; a++ {
+		if len(params[a]) <= 0 {
+			return shim.Error("Argument must be a non-empty string")
+		}
+	}
+
+	// Copy the Values from params[]
+	RegNo := params[0]
+	Insurance := params[1]
+
+	// Check if Vehicle exists with Key => RegNo
+	vehicleAsBytes, err := stub.GetState(RegNo)
+	if err != nil {
+		return shim.Error("Failed to get Vehicle Details!")
+	} else if vehicleAsBytes == nil {
+		return shim.Error("Error: Vehicle Does NOT Exist!")
+	}
+
+	// Create Update struct var
+	vehicleToUpdate := vehicle{}
+	err = json.Unmarshal(vehicleAsBytes, &vehicleToUpdate) //unmarshal it aka JSON.parse()
+	if err != nil {
+		return shim.Error(err.Error())
+	}
+
+	// Update Vehicle.PollutionCert to append => Cert
+	vehicleToUpdate.Insurance = append(vehicleToUpdate.Insurance, Insurance)
+
+	// Get JSON bytes of Vehicle struct
+	vehicleJSONasBytes, err := json.Marshal(vehicleToUpdate)
+	if err != nil {
+		return shim.Error(err.Error())
+	}
+
+	// Put State of newly generated Vehicle with Key => params[0]
+	err = stub.PutState(RegNo, vehicleJSONasBytes)
+	if err != nil {
+		return shim.Error(err.Error())
+	}
+
+	// Returned on successful execution of the function
+	return shim.Success(nil)
+}
+
+// Function to add Transfer Ownership
+func (cc *Chaincode) transferOwnership(stub shim.ChaincodeStubInterface, params []string) sc.Response {
+	// Check Access
+	creatorOrg, creatorCertIssuer, creator, err := getTxCreatorInfo(stub)
+	if !authenticateRTO(creatorOrg, creatorCertIssuer) {
+		return shim.Error("{\"Error\":\"Access Denied!\",\"Payload\":{\"MSP\":\"" + creatorOrg + "\",\"CA\":\"" + creatorCertIssuer + "\"}}")
+	}
+
+	// Check if sufficient Params passed
+	if len(params) != 3 {
+		return shim.Error("Incorrect number of arguments. Expecting 9")
+	}
+
+	// Check if Params are non-empty
+	for a := 0; a < 3; a++ {
+		if len(params[a]) <= 0 {
+			return shim.Error("Argument must be a non-empty string")
+		}
+	}
+
+	// Copy the Values from params[]
+	RegNo := params[0]
+	Curr := params[1]
+	Date := params[1]
+	DateI, err := strconv.Atoi(Date)
+	if err != nil {
+		return shim.Error("Error: Invalid Date!")
+	}
+
+	// Check if Vehicle exists with Key => RegNo
+	vehicleAsBytes, err := stub.GetState(RegNo)
+	if err != nil {
+		return shim.Error("Failed to get Vehicle Details!")
+	} else if vehicleAsBytes == nil {
+		return shim.Error("Error: Vehicle Does NOT Exist!")
+	}
+
+	// Create Update struct var
+	vehicleToUpdate := vehicle{}
+	err = json.Unmarshal(vehicleAsBytes, &vehicleToUpdate) //unmarshal it aka JSON.parse()
+	if err != nil {
+		return shim.Error(err.Error())
+	}
+
+	Record := ownership{vehicleToUpdate.Owner, Curr, creator, DateI}
+
+	// Update Vehicle.OwnershipHistory to append => Record
+	vehicleToUpdate.OwnershipHistory = append(vehicleToUpdate.OwnershipHistory, Record)
+	// Update Vehicle.Owner = Curr
+	vehicleToUpdate.Owner = Curr
+
+	// Get JSON bytes of Vehicle struct
+	vehicleJSONasBytes, err := json.Marshal(vehicleToUpdate)
+	if err != nil {
+		return shim.Error(err.Error())
+	}
+
+	// Put State of newly generated Vehicle with Key => params[0]
+	err = stub.PutState(RegNo, vehicleJSONasBytes)
+	if err != nil {
+		return shim.Error(err.Error())
+	}
+
+	// Returned on successful execution of the function
+	return shim.Success(nil)
 }
 
 // ---------------------------------------------
@@ -146,7 +351,7 @@ func (cc *Chaincode) readEvidence(stub shim.ChaincodeStubInterface, params []str
 // ++++++++++++++
 
 // Get Tx Creator Info
-func getTxCreatorInfo(stub shim.ChaincodeStubInterface) (string, string, error) {
+func getTxCreatorInfo(stub shim.ChaincodeStubInterface) (string, string, string, error) {
 	var mspid string
 	var err error
 	var cert *x509.Certificate
@@ -154,19 +359,19 @@ func getTxCreatorInfo(stub shim.ChaincodeStubInterface) (string, string, error) 
 
 	if err != nil {
 		fmt.Printf("Error getting MSP identity: %sn", err.Error())
-		return "", "", err
+		return "", "", "", err
 	}
 
 	cert, err = cid.GetX509Certificate(stub)
 	if err != nil {
 		fmt.Printf("Error getting client certificate: %sn", err.Error())
-		return "", "", err
+		return "", "", "", err
 	}
 
-	return mspid, cert.Issuer.CommonName, nil
+	return mspid, cert.Issuer.CommonName, cert.Subject.CommonName, nil
 }
 
-// Authenticate => Police / Forensics / Citizen
-func authenticatePoFoCi(mspID string, certCN string) bool {
-	return (mspID == "PoliceMSP") && (certCN == "ca.police.example.com") || (mspID == "ForensicsMSP") && (certCN == "ca.forensics.example.com") || (mspID == "CitizenMSP") && (certCN == "ca.citizen.example.com")
+// Authenticate => RTO
+func authenticateRTO(mspID string, certCN string) bool {
+	return (mspID == "RTOMSP") && (certCN == "ca.rto.vehicle.com")
 }
