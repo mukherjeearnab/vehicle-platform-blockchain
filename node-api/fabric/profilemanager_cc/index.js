55e2336a113bb012f3f47bd04a5b28cce690e972
Arnab Mukherjee <mukherjeearnab905@gmail.com>
2020-08-05 09:53:54 +0530

    resolved incomplete index.js

--- node-api/fabric/profilemanager_cc/index.js
+++ node-api/fabric/profilemanager_cc/index.js
@@ -2,12 +2,28 @@ const GetProfile = require("./getProfile");
 const RegisterCitizen = require("./registerCitizen");
 const AddVehicle = require("./addVehicle");
 const AddLicense = require("./addLicense");
+const RegisterRto = require("./registerRto");
+const RegisterRtoE = require("./registerRtoE");
+const RegisterInsuranceCompany = require("./registerInsuranceCompany");
+const RegisterInsuranceE = require("./registerInsuranceE");
+const RegisterPollutionCompany = require("./registerPollutionCompany");
+const RegisterPollutionE = require("./registerPollutionE");
+const RegisterPoliceStation = require("./registerPoliceStation");
+const RegisterPoliceOfficer = require("./registerPoliceOfficer");
 
 const payload = {
     GetProfile,
     RegisterCitizen,
     AddVehicle,
     AddLicense,
+    RegisterRto,
+    RegisterRtoE,
+    RegisterInsuranceCompany,
+    RegisterInsuranceE,
+    RegisterPollutionCompany,
+    RegisterPollutionE,
+    RegisterPoliceStation,
+    RegisterPoliceOfficer,
 };
 
 module.exports = payload;
